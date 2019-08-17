a4bd7398966edc5a882e3c323e3a60e1729d15f5
Thijs van den Berg <thijs@mountainwebs.nl>
2019-08-17 15:15:55 +0200

    Initial version

---
+++ DruidBarSlim.lua
@@ -0,0 +1,32 @@
+local POWER_MANA = 0
+local FORM_BEAR = 1
+local FORM_CAT = 2
+
+function DruidBarSlim_OnLoad()
+    DruidBarSlim_Frame:RegisterEvent("UNIT_DISPLAYPOWER");
+    ShowOrHide();
+    DruidBarSlim_OnRefresh();
+end
+
+function DruidBarSlim_OnEvent(self, event, ...)
+    if event == "UNIT_DISPLAYPOWER" then
+        ShowOrHide();
+    end
+end
+
+function DruidBarSlim_OnRefresh()
+    DruidBarSlim_Frame_Mana:SetMinMaxValues(0, UnitPowerMax("player", POWER_MANA));
+    DruidBarSlim_Frame_Mana:SetValue(UnitPower("player", POWER_MANA));
+    DruidBarSlim_Frame_Mana:SetStatusBarColor(0, 0, 1, 1);
+    local str = UnitPower("player", POWER_MANA) .. " / " .. UnitPowerMax("player", POWER_MANA);
+    DruidBarSlim_Frame_Text:SetText(str);
+end
+
+function ShowOrHide()
+    local form = GetShapeshiftForm();
+    if form == FORM_BEAR or form == FORM_CAT then
+        DruidBarSlim_Frame:Show();
+    else
+        DruidBarSlim_Frame:Hide();
+    end
+end
